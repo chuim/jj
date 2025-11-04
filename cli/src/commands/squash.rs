0037b87670b5e693d5f27482a02bea26a31baad1
Scott Taylor <scott11x8@gmail.com>
2025-11-03 18:06:57 -0600

    merged_tree: use `Merge<TreeId>` instead of `Merge<Tree>`
    Currently, creating a `MergedTree` requires reading all of its root
    trees from the store. However, this is often not actually required. For
    instance, if the only reason to read the trees is to call
    `MergedTree::merge`, and the merge is trivial, then there was no need to
    read the trees. Changing `MergedTree` to only require a `Merge<TreeId>`
    instead of a `Merge<Tree>` will make it possible to avoid reading trees
    unnecessarily in these cases.
    
    One benefit of this approach is that `Commit::tree` no longer requires
    reading from the store, so it can be made synchronous and infallible,
    which simplifies a lot of code.

--- cli/src/commands/squash.rs
+++ cli/src/commands/squash.rs
@@ -459,7 +459,7 @@ fn select_diff(
     let mut source_commits = vec![];
     for source in sources {
         let parent_tree = source.parent_tree(tx.repo())?;
-        let source_tree = source.tree()?;
+        let source_tree = source.tree();
         let format_instructions = || {
             formatdoc! {"
                 You are moving changes from: {source}
